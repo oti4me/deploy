dbe2da31f83504da1c039355502c41d677d75dee
Henry Otighe <henry.otighe@construyo.de>
2021-07-19 11:01:36 +0200

    another commit

--- src/hooks/useGraphqlQuery.js
+++ src/hooks/useGraphqlQuery.js
@@ -4,6 +4,6 @@ import { request } from 'graphql-request';
 
 export default function useGraphqlQuery(query, variables) {
   return useSWR([query, variables], (query, variables) =>
-    request('my-api/graphql', query, variables)
+    request('/my-api/graphql', query, variables)
   );
 }
